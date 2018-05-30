2fa6fdf7907595b4bac4447668fd90933854a0b9
Radik <kaddafi88@gmail.com>
2018-05-30 23:28:00 +0500

    Это наш пятый коммит

--- lesson_1_hard/js/script.js
+++ lesson_1_hard/js/script.js
@@ -1,32 +1,27 @@
-var num = 33721;
+var names = 33721;
+var op = 1;
 
+var arr = names.toString().split('');
 
-mainList = {
-  budget : Number,
-  nameStore : String,
-  shopGoods : ["One", "Two", "Three"],
-  employers : {
-  },
-  open : true
-};
+for (var i = 0; i < arr.length; i++) {
+  // console.log( 'Вам сообщение ' + arr[i] );
+  op = op * +arr[i];
+  // console.log( 'Вам сообщение 1 ' + op);
+}
 
+console.log( 'Произведение числа ' + op );
 
-mainList.budget = +prompt("Ваш бюджет за месяц?");
-mainList.nameStore = prompt("Название Вашего магазина?");
+var degree = Math.pow(op,3)
 
-mainList.shopGoods[0] = prompt("Какой тип товара будем продавать? первый");
-mainList.shopGoods[1] = prompt("Какой тип товара будем продавать? второй");
-mainList.shopGoods[2] = prompt("Какой тип товара будем продавать? третий");
+console.log( 'Степень 3 ' + degree );
 
+var arr1 = degree.toString().split('');
+var op1 = '';
 
- 
+for (var i = 0; i < 2; i++) {
+  op1 = op1 + arr1[i];
+}
 
-console.log("Тут бюджет " + mainList.budget);
-console.log("Тут название магазина " + mainList.nameStore);
-console.log("Тип товаров " + mainList.shopGoods[0] + " | " 
-  + mainList.shopGoods[1] + " | " + mainList.shopGoods[2] + " | " );
+console.log( 'Первые две цифры ' + op1 );
 
-alert("Бюджет на день: " + mainList.budget / 30);
-
-console.log("Проверка типа " + typeof(mainList.open));
-
+alert('Первые две цифры ' + op1);
