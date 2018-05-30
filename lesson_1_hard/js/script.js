72c47b737feeab5cad1e8fa962554dfe44838d46
Radik <kaddafi88@gmail.com>
2018-05-30 17:22:05 +0500

    Это нам первый комит

---
+++ lesson_1_hard/js/script.js
@@ -0,0 +1,32 @@
+var num = 33721;
+
+
+mainList = {
+  budget : Number,
+  nameStore : String,
+  shopGoods : ["One", "Two", "Three"],
+  employers : {
+  },
+  open : true
+};
+
+
+mainList.budget = +prompt("Ваш бюджет за месяц?");
+mainList.nameStore = prompt("Название Вашего магазина?");
+
+mainList.shopGoods[0] = prompt("Какой тип товара будем продавать? первый");
+mainList.shopGoods[1] = prompt("Какой тип товара будем продавать? второй");
+mainList.shopGoods[2] = prompt("Какой тип товара будем продавать? третий");
+
+
+ 
+
+console.log("Тут бюджет " + mainList.budget);
+console.log("Тут название магазина " + mainList.nameStore);
+console.log("Тип товаров " + mainList.shopGoods[0] + " | " 
+  + mainList.shopGoods[1] + " | " + mainList.shopGoods[2] + " | " );
+
+alert("Бюджет на день: " + mainList.budget / 30);
+
+console.log("Проверка типа " + typeof(mainList.open));
+
---
+++ lesson_1_hard/js/tst.js
@@ -0,0 +1,77 @@
+
+
+
+
+var leftBorderWidth = 1;
+
+const pi = 3.14;
+
+{
+  let second = 2;  
+}
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string' * 9);
+
+// console.log(n1);
+
+var und;
+console.log(und);
+
+obj = {
+  name: "John",
+  age: 25,
+  isMarried: false
+}
+
+// console.log(obj.name);
+console.log(obj["name"]);
+
+var arr = ['plum', 'orange', 'apple'];
+console.log(arr[2]);
+
+// alert("hello!")
+// confirm("Are you here?");
+// var answer = +prompt("Вам есть 18?", "18");
+
+
+// console.log(typeof(answer));
+
+// console.log('arr' + " - object");
+// console.log('4' + " - object");
+
+var incr = 10,
+    decr = 10;
+
+  // incr++;
+  // decr--;
+
+  // console.log(incr++);
+  // console.log(decr--);
+
+  console.log(5%2);
+
+  console.log(incr == 10); //простое сравнение
+  console.log("10" === 10); // сравнение так же и по типу данных
+
+  var isChecked = true;
+  var isClose = true;
+
+  console.log(isChecked || !isClose);
+
+
+
+
+
+  var budget = +prompt("Ваш буджет на месяц?");
+  var nameStore = prompt("Название Вашего магазина?");
+
+  
